ce193051b38a7411e75e251c6282b5c63be4c3dd
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2021-02-17 07:10:31 +0000

    Merge in sync (pull request #1)
    Sync
    
    * figure out where it is falling out of sync
    
    * rm synchronization attempt
    
    This actually seems to work just fine, but I'm not confident it would
    recover if it got out of sync.
    
    * this behaves differently than a938873 -- timing?

--- bin/radiometer_lcmd.py
+++ bin/radiometer_lcmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""flowmeter_can_lcmd - LCM daemon for flow meter."""
+"""radiometer_lcmd - LCM daemon for radiometer."""
 
 import select
 import serial
@@ -17,12 +17,12 @@ class RadiometerDaemon:
 
     def __init__(self, dev='/dev/ttyUSB1', prefix='RAD'):
         """Define serial and LCM interfaces, and subscribe to input."""
-        self.serial = serial.Serial(dev, baudrate=38400, timeout=0.1)
+        self.serial = serial.Serial(dev, baudrate=38400, timeout=1)
         self.lcm = lcm.LCM()
         self.prefix = prefix
-        self.hdr = deque(maxlen = 4)
-        self.hrtpkt = struct.Struct('<5L') # 5 unsigned long bytes (UTC, Pulse count, nsHI, irradiance, end token)
-        self.datpkt = struct.Struct('<2L50H') # 2 unsigned long bytes (ISR clock cycles, LOG clock cycles)
+        self.tkn = deque(maxlen = 4)
+        self.heartbeat = struct.Struct('<5L') # 5 uint32 (UTC, Pulse count, nsHI, irradiance, end token)
+        self.data = struct.Struct('<2L50H') # 2 uint32 (ISR clock cycles, LOG clock cycles), then 50 uint16
 
         self.subscriptions = []
         self.subscriptions.append(self.lcm.subscribe(
@@ -41,36 +41,32 @@ class RadiometerDaemon:
 
     def serial_handler(self):
         """Receive data on serial port and send on LCM."""
-        self.hdr.clear()
-        self.hdr.extend(self.serial.read(4))
-        while(self.serial.in_waiting > 0):
-            # assert (len(self.hdr) == self.hdr.maxlen), "header too short"
-            bsh = bytes(self.hdr)
-            if bsh == b'\x00\xff\x00\xff':
+        pkt_size = 0
+        self.tkn.extend(self.serial.read(self.tkn.maxlen))
+        while pkt_size == 0:
+            bsh = bytes(self.tkn)
+            if bsh == bytes.fromhex('ff00ff00'):
                 suffix = 'o'
-                pkt_size = self.datpkt.size
-            elif bsh == b'\x00\xfe\x00\xfe':
+                pkt_size = self.data.size
+            elif bsh == bytes.fromhex('fe00fe00'):
                 suffix = 'h'
-                pkt_size = self.hrtpkt.size
+                pkt_size = self.heartbeat.size
             else:
                 suffix = 'r'
                 pkt_size = 0
-                print(bytes(self.hdr).hex(' '))
-
             self.handle_pkt(suffix, pkt_size)
-            if pkt_size == 0:
-                print(' <<=')
-                self.hdr.extend(self.serial.read(1))
-                print(bytes(self.hdr).hex(' '))
-                print('____')
+            if pkt_size == 0 and self.serial.in_waiting > 0:
+                self.tkn.extend(self.serial.read(1))
 
-    def handle_pkt(self, suffix='r', sz=108):
+    def handle_pkt(self, suffix='r', sz=0):
+        print("handling packet with header: {0}, size: {1}".format(
+            bytes(self.tkn).hex(), sz))
         rx = self.serial.read(sz)
         if(len(rx) == sz):
             tx = bytes_t()
             tx.utime = int(time.time() * 1e6)
-            tx.length = len(self.hdr) + sz
-            tx.data = bytes(self.hdr) + rx
+            tx.length = len(self.tkn) + sz
+            tx.data = bytes(self.tkn) + rx
             self.lcm.publish("{0}{1}".format(self.prefix, suffix), tx.encode())
         else:
             print('tried to read {0} but only got {1}'.format(sz, len(rx)))
@@ -80,6 +76,8 @@ class RadiometerDaemon:
         epoll = select.epoll()
         epoll.register(self.lcm.fileno(), select.EPOLLIN)
         epoll.register(self.serial.fileno(), select.EPOLLIN)
+        self.serial.reset_input_buffer()
+        self.serial.reset_output_buffer()
         try:
             while True:
                 for fileno, _events in epoll.poll(1):
