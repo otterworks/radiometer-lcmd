b2fe6676f8431c53cc51c4d00b25bc00f3ec52cd
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2021-02-27 10:13:59 -0800

    switch byte order in packet tokens to match emulator

--- bin/radiometer_lcmd.py
+++ bin/radiometer_lcmd.py
@@ -45,10 +45,10 @@ class RadiometerDaemon:
         self.tkn.extend(self.serial.read(self.tkn.maxlen))
         while pkt_size == 0:
             bsh = bytes(self.tkn)
-            if bsh == bytes.fromhex('80008000'):
+            if bsh == bytes.fromhex('00800080'):
                 suffix = 'o'
                 pkt_size = self.data.size
-            elif bsh == bytes.fromhex('81118111'):
+            elif bsh == bytes.fromhex('11811181'):
                 suffix = 'h'
                 pkt_size = self.heartbeat.size
             else:
