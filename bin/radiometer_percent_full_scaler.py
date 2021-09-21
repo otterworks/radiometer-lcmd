7d5b7e39842d4d76a8dad25af3ba7e84a22e95a0
M J Stanway <m.j.stanway@alum.mit.edu>
2021-09-21 22:26:51 +0000

    add a simple scaler for %FS signal

---
+++ bin/radiometer_percent_full_scaler.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+import select
+import serial
+import struct
+import time
+import lcm
+
+from radiometer_lcmtypes.raw import floats_t
+
+
+class PercentFullScaler:
+
+    def __init__(self, verbose=0):
+        self.lcm = lcm.LCM()
+        self.verbose = verbose
+
+    def handler(self, channel, data):
+        """receive time high and publish percent 
+        """
+        rx = floats_t.decode(data)
+        rx.data *= 16e-4;
+        self.lcm.publish("{0}p".format(channel), rx.encode)
+
+    def filter(self, channel='RAD1t'):
+        """Connect to LCM and handle."""
+
+        subscription = self.lcm.subscribe(channel, self.handler)
+        try:
+            while True:
+                self.lcm.handle()
+        except (KeyboardInterrupt, SystemExit):
+            print('stopped by user')
+            subscription.unsubscribe()
+
+
+def main(channel='RAD1t', verbose=0):
+    """Run as a daemon."""
+    pfs = PercentFullScaler(verbose=verbose)
+    pfs.filter(channel);
+
+
+if __name__ == "__main__":
+    import argparse
+    p = argparse.ArgumentParser(description="LCM daemon to filter radiometer data")
+    p.add_argument('-v', '--verbose', action='count', default=0,
+                   help='display verbose output')
+    p.add_argument('-V', '--version', action='version',
+                   version='%(prog)s 0.0.1',
+                   help='display version information and exit')
+    p.add_argument('-c', '--channel', default='RAD1t',
+                   help='channel to listen on')
+
+    a = p.parse_args()
+    main(**a.__dict__)
